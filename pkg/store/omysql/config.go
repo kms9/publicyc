b6ffd5541f2414f30e7537c8f1e0af0e2bd259cf
code.sun <code.sun@qq.com>
2021-03-14 01:18:05 +0800

    add mysql

---
+++ pkg/store/omysql/config.go
@@ -0,0 +1,101 @@
+package opg
+
+import (
+	"time"
+
+	"github.com/kms9/publicyc/pkg/conf"
+	onion_log "github.com/kms9/publicyc/pkg/onion-log"
+	"github.com/kms9/publicyc/pkg/util/otime"
+
+	//"github.com/kms9/publicyc/pkg/util/otime"
+)
+
+// config options
+type Config struct {
+	Name string
+	// 连接地址
+	Host string `json:"host"`
+	// 端口
+	Port int `json:"port"`
+	// 用户名
+	User string `json:"user"`
+	// 密码
+	Password string `json:"password"`
+	// 数据库名称
+	Db string `json:"db"`
+
+	// 最大空闲连接数
+	MaxIdleConns int `json:"maxIdleConns"`
+	// 最大活动连接数
+	MaxOpenConns int `json:"maxOpenConns"`
+	// 连接的最大存活时间
+	ConnMaxLifetime time.Duration `json:"connMaxLifetime"`
+	// 慢日志阈值
+	SlowThreshold time.Duration `json:"slowThreshold"`
+	// 关闭指标采集
+	DisableMetric bool `json:"disableMetric"`
+	// Debug开关
+	Debug         bool `json:"debug"`
+
+	// 日志
+	_logger *onion_log.Log
+	// sql回调函数
+	interceptors []Interceptor
+
+}
+
+// UseConfig 标准配置，规范配置文件头
+func UseConfig(name string) *Config {
+	return RawConfig("yc.mysql." + name)
+}
+
+// RawConfig 解析配置
+func RawConfig(key string) *Config {
+	var config = DefaultConfig()
+	if conf.Detail().Get(key) == nil {
+		onion_log.Panicf("key:%s mysqlConfig is not exists", key)
+	}
+	if err := conf.Detail().UnmarshalKey(key, config); err != nil {
+		onion_log.Panicf("unmarshal mysqlConfig key:%s err:%s", key, err)
+	}
+
+	config.Name = key
+	return config
+}
+
+// DefaultConfig 返回默认配置
+func DefaultConfig() *Config {
+	return &Config{
+		Debug:           false,
+		MaxIdleConns:    10,
+		MaxOpenConns:    100,
+		ConnMaxLifetime: otime.Duration("300s"),
+		SlowThreshold:   otime.Duration("500ms"),
+		_logger:         onion_log.DefaultLogger(),
+	}
+}
+
+// WithInterceptor ...
+func (c *Config) WithInterceptor(intes ...Interceptor) *Config {
+	if c.interceptors == nil {
+		c.interceptors = make([]Interceptor, 0)
+	}
+	c.interceptors = append(c.interceptors, intes...)
+	return c
+}
+
+// Build ...
+func (c *Config) Build() *DB {
+	if !c.DisableMetric {
+		c = c.WithInterceptor(metricInterceptor)
+	}
+	db, err := Open(c)
+	if err != nil {
+		c._logger.Panicf("connect mysql err %s", err)
+	}
+
+	if err := db.DB().Ping(); err != nil {
+		c._logger.Panicf("ping mysql err %s", err)
+	}
+	return db
+}
---
+++ pkg/store/omysql/db.go
@@ -0,0 +1,78 @@
+package opg
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/kms9/publicyc/pkg/util"
+)
+
+type (
+	DB = gorm.DB
+	// Scope ...
+	Scope = gorm.Scope
+)
+
+// Open 连接数据库
+func Open(c *Config) (*DB, error) {
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", c.User,  c.Password, c.Host, c.Port, c.Db)
+	db, err := gorm.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if util.GetGoEnv() == "development" {
+		db.LogMode(true)
+	} else {
+		db.LogMode(c.Debug)
+	}
+
+	db.SingularTable(true)
+	db.DB().SetMaxIdleConns(c.MaxIdleConns)
+	db.DB().SetMaxOpenConns(c.MaxOpenConns)
+	if c.ConnMaxLifetime != 0 {
+		db.DB().SetConnMaxLifetime(c.ConnMaxLifetime)
+	}
+
+	c._logger.Infof("mysql: %s connect: %s MaxIdleConns=%d MaxOpenConns=%d ConnMaxLifetime=%ds", c.Name, dsn, c.MaxIdleConns, c.MaxOpenConns, c.ConnMaxLifetime/time.Second)
+	replace := func(processor func() *gorm.CallbackProcessor, callbackName string, interceptors ...Interceptor) {
+		old := processor().Get(callbackName)
+		var handler = old
+		for _, inte := range interceptors {
+			handler = inte(callbackName, c)(handler)
+		}
+		processor().Replace(callbackName, handler)
+	}
+
+	replace(
+		db.Callback().Delete,
+		"gorm:delete",
+		c.interceptors...,
+	)
+	replace(
+		db.Callback().Update,
+		"gorm:update",
+		c.interceptors...,
+	)
+	replace(
+		db.Callback().Create,
+		"gorm:create",
+		c.interceptors...,
+	)
+	replace(
+		db.Callback().Query,
+		"gorm:query",
+		c.interceptors...,
+	)
+	replace(
+		db.Callback().RowQuery,
+		"gorm:row_query",
+		c.interceptors...,
+	)
+
+	return db, nil
+}
